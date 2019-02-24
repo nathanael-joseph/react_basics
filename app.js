7c92f68c0cea45f6dbbe09da1520a1f3b2d13556
nathan yebgui <nathan_yebgui@msn.com>
2019-02-24 21:56:59 +0200

    created second component as arrow function.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// component as funtion
 function Header () {
     return (
         <header>
@@ -6,8 +7,22 @@ function Header () {
         </header>
     );
 }
-
+// component as arrow function.
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">
+                Nathan
+            </span>
+            <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <span className="counter-score">35</span>
+                <button className="counter-action increment"> + </button>
+            </div>
+        </div>
+    );
+}
 ReactDOM.render(
-<Header />,
-document.getElementById("root")
-);
+[<Header />, <Player />],
+    document.getElementById("root")
+    );

bb42608d9a6498dd08855c1aa5bfc9edfbe2fe79
nathan yebgui <nathan_yebgui@msn.com>
2019-03-03 10:33:30 +0200

    added seperate Counter component and top level App component

--- app.js
+++ app.js
@@ -13,16 +13,31 @@ const Player = () => {
         <div className="player">
             <span className="player-name">
                 Nathan
-            </span>
+            </span>  
+            <Counter />          
+        </div>
+    );
+}
+const Counter = () => {
+    return(
             <div className="counter">
                 <button className="counter-action decrement"> - </button>
                 <span className="counter-score">35</span>
                 <button className="counter-action increment"> + </button>
             </div>
+    );
+}
+
+const App = () => {
+    return (
+        <div className="scoreBoard">
+            <Header />
+            {/* Players list */}
+            <Player />
         </div>
     );
 }
 ReactDOM.render(
-[<Header />, <Player />],
+    <App />,
     document.getElementById("root")
     );

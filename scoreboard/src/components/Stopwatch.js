d096ef9a285c4f33e36eddab7492117ab0c13b46
nathan yebgui <nathan_yebgui@msn.com>
2019-04-21 15:54:43 +0300

    added stopwatch component

---
+++ scoreboard/src/components/Stopwatch.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class Stopwatch extends Component {
+    render() {
+        return (
+            <div className="stopwatch">
+                <h2>Stopwatch</h2>
+                <span className="stopwatch-time">15</span>
+                <button>Start</button>
+                <button>Reset</button>
+            </div>
+        );
+    }
+}
+
+export default Stopwatch;
